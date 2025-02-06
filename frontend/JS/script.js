7d40692ef8b6895325e5c55867664d959c4c4ba9
MeronM18 <matti2@oakland.edu>
2025-02-06 00:11:30 -0500

    Co-authored-by: Jasmanpreet Singh <Jasman047@users.noreply.github.com>
    login page design

---
+++ frontend/JS/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const currentPage = window.location.pathname;
+  const menuItems = document.querySelectorAll('.menu-items');
+
+  menuItems.forEach(function(item){
+    const link = item.getAttribute('href');
+    if (link == currentPage) {
+      item.classList.add('active');
+    }
+  })
+})
+
+/********************************************************* */
+
+const toggleButton = document.getElementById('toggle-btn');
+toggleButton.addEventListener("click", openSideBar);
+
+function openSideBar() {
+  const navbar = document.querySelector('nav');
+  navbar.classList.toggle('show');
+}
+
+/********************************************************* */
+const closeButton = document.getElementById('close-btn');
+closeButton.addEventListener("click", closeSideBar);
+
+function closeSideBar(){
+  const navbarshow = document.querySelector('nav');
+  navbarshow.classList.remove('show');
+}
+
+/********************************************************* */
+const submenuButton = document.getElementById('submenu-btn');
+submenuButton.addEventListener("click", toggleSideMenu);
+
+function toggleSideMenu() {
+  const submenu = document.querySelector('.sub-menu');
+  submenu.classList.toggle('show');
+
+  submenuButton.classList.toggle('downarrow-icon')
+}
