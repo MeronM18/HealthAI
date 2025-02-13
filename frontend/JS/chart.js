7ce69a3f8668518163369505da1305bf30ecf738
MeronM18 <matti2@oakland.edu>
2025-02-13 15:34:13 -0500

    graph design

--- frontend/JS/chart.js
+++ frontend/JS/chart.js
@@ -1,22 +1,124 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById('myChart').getContext('2d');
 
   new Chart(ctx, {
-    type: 'scatter',
+    type: 'bar',
     data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: 'Scatter',
-        data: [{x:-10,y:0}],
+        label: 'Weight',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgb(235, 64, 52)',
+          'rgb(22, 84, 184)',
+          'rgb(224, 218, 31)',
+          'rgb(27, 204, 35)',
+          'rgb(129, 20, 184)',
+          'rgb(250, 151, 2)'
+        ],
+        borderColor: 'rgb(255,255,255)',
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ctx = document.getElementById('myChart2').getContext('2d');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgb(235, 64, 52)',
+          'rgb(22, 84, 184)',
+          'rgb(224, 218, 31)',
+          'rgb(27, 204, 35)',
+          'rgb(129, 20, 184)',
+          'rgb(250, 151, 2)'
+        ],
         borderWidth: 1
       }]
     },
     options: {
       scales: {
-        x: {
-          type: 'linear',
-          position: 'bottom'
+        y: {
+          beginAtZero: true
         }
       }
     }
   });
-};
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ctx = document.getElementById('myChart3').getContext('2d');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ctx = document.getElementById('myChart4').getContext('2d');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(153, 102, 255)',
+          'rgb(255, 159, 64)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
